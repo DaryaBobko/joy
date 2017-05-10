003f832729bb9814093787b977ba4ccbcb6c6203
Darya <Bobko.dv@gmail.com>
2017-05-10 21:08:26 +0300

    no message

--- joyEgine/DiplomWEB/Pages/footer/footer-controller.js
+++ joyEgine/DiplomWEB/Pages/footer/footer-controller.js
@@ -3,8 +3,7 @@
 
     module.controller('FooterController', footerController);
     function footerController() {
-       // var vm = this;
-      //  vm.tags = [ "Спорт", "Кухня", "Юмор", "Отдых" ];
+      
     }
 })();
 
