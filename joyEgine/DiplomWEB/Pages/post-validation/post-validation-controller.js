0a83a851f4bafa5564a9f9d9b9febbda58d5740d
Egor Garkavy <supergarich@gmail.com>
2017-05-21 13:35:04 +0300

    редактирование поста

---
+++ joyEgine/DiplomWEB/Pages/post-validation/post-validation-controller.js
@@ -0,0 +1,20 @@
+﻿angular.module('DiplomApp').controller('PostValidationController', postValidationController);
+
+postValidationController.$inject = [];
+
+function postValidationController() {
+    var vm = this;
+
+    vm.UserInfo = {};
+
+    vm.actions = {
+    };
+
+
+    init();
+
+    function init() {
+    }
+
+
+}
---
+++ joyEgine/DiplomWEB/Pages/post-validation/post-validation.js
@@ -0,0 +1,18 @@
+﻿/// <reference path="Main.html" />
+(function () {
+    "use strict";
+    angular
+        .module("DiplomApp")
+        .config(config);
+
+    config.$inject = ["$stateProvider"];
+
+    function config($stateProvider) {
+        $stateProvider
+            .state("post-validation", {
+                url: "/post-validation/:id",
+                templateUrl: "/Pages/post-validation/post-validation.tmpl.html",
+                controller: "PostValidationController as vm"
+            });
+    }
+})();
