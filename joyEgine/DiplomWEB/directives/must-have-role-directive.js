bc283a463c802970625cb109d88f09083cc065a9
Egor Garkavy <supergarich@gmail.com>
2017-05-29 11:13:26 +0300

    роли для приложения

---
+++ joyEgine/DiplomWEB/directives/must-have-role-directive.js
@@ -0,0 +1,34 @@
+﻿angular
+    .module('DiplomApp')
+    .directive('mustHaveRole', mustHaveRole);
+
+mustHaveRole.$inject = ['userService', "ngIfDirective"];
+
+function mustHaveRole(userService, ngIfDirective) {
+    var ngIf = ngIfDirective[0];
+
+    var directive = {
+        transclude: ngIf.transclude,
+        priority: ngIf.priority + 1,
+        terminal: ngIf.terminal,
+        restrict: ngIf.restrict,
+        scope: {
+            mustHaveRole: "="
+        },
+        link: link
+    };
+
+    return directive;
+
+    function link(scope, element, attr) {
+        var isApprove = false;
+        if (userService.isInnRole(scope.mustHaveRole)) {
+            isApprove = true;
+        }
+
+        attr.ngIf = function () {
+            return isApprove;
+        };
+        ngIf.link.apply(ngIf, arguments);
+    }
+}
